4f669adbe4d084df1eba58334ae0c446b8ba6ab5
Grzegorz Oleksa <grzesieq92@gmail.com>
2023-10-08 16:27:52 +0200

    app changed

--- src/App.js
+++ src/App.js
@@ -1,30 +1,7 @@
-const CurrencyConverter = () => {
+function App() {
+ return (
+  <h1>napis</h1>
+ )
+}
 
-
-  return (
-    <div>
-      <h1>Przelicznik walut - test</h1>
-      <form>
-        <label>
-          kwota w zl:
-          <input />
-        </label>
-        <p>
-          <label>
-            waluta:
-            <select>
-              <option>eur</option>
-              <option>usd</option>
-            </select>
-          </label>
-        </p>
-
-        <button>przelicz</button>
-      </form> 
-
-      
-    </div>
-  );
-};
-
-export default CurrencyConverter;
+export default App;
